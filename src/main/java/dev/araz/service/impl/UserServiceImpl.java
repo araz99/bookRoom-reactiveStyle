b3df6bdcf72a3e333e3342807761ef6466e99f24
Araz <mr.muhammed.araz@gmail.com>
2021-05-05 14:09:29 +0300

    release_v1.0

--- src/main/java/dev/araz/service/impl/UserServiceImpl.java
+++ src/main/java/dev/araz/service/impl/UserServiceImpl.java
@@ -16,6 +16,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(UserDto userDto) {
+//        userRepository.findUserByUsername(userDto.getUsername());
         userRepository.save(mapper.toEntity(userDto)).subscribe();
     }
 }
